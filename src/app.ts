1bb2c679727698b4b727eb417a4666cc538ed195
Behzad <ruhollah.a.d@gmail.com>
2026-05-14 21:43:18 +0200

    Add MongoDB connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("ecommerce-api is running");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
